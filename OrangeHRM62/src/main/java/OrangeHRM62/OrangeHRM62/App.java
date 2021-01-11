a622254ea4a54f0f889cf89eca9a0fda408c9256
Lenovo <Lenovo@DESKTOP-CQPKTR4>
2021-01-11 09:39:44 +0530

    pusing on jan 11

---
+++ OrangeHRM62/src/main/java/OrangeHRM62/OrangeHRM62/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM62.OrangeHRM62;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
